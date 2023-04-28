6326ba46a8658616fec5160455ec22a1d6b2b8b9
GulenkinID <gulenkini@yandex.ru>
2023-04-28 13:11:18 +0700

    create declOfNum function

--- js/components/createCategory.js
+++ js/components/createCategory.js
@@ -1,4 +1,5 @@
 import { createElement } from "../helper/createElement.js"
+import { declOfNum } from "../helper/declOfNum.js";
 
 export const createCategory = (app) => {
 	const category = createElement('section', {
@@ -31,7 +32,7 @@ export const createCategory = (app) => {
 		});
 		const elementSpanPairs = createElement('span', {
 			className: 'category__pairs',
-			textContent: `${data.length} пар`,
+			textContent: declOfNum(data.length, ['пара', 'пары', 'пар']),
 		});
 		elementBtn.append(elementSpanTitle, elementSpanPairs);
 
@@ -67,47 +68,3 @@ export const createCategory = (app) => {
 		categoryList,
 	}
 }
-
-/*
-<section class="category section-offset">
-      <div class="container">
-        <categoryList class="category__list">
-          <li class="category__item" data-id="bc2iv1cwi6ht">
-            <button class="category__card">
-              <span class="category__title">Косвенные местоимения</span>
-              <span class="category__pairs">7 пар</span>
-            </button>
-            <button class="category__btn category__edit" aria-label="редактировать"></button>
-            <button class="category__btn category__del" aria-label="удалить"></button>
-          </li>
-
-          <li class="category__item" data-id="bcbop5qmm9sy">
-            <button class="category__card">
-              <span class="category__title">Описание внешности</span>
-              <span class="category__pairs">26 пар</span>
-            </button>
-            <button class="category__btn category__edit" aria-label="редактировать"></button>
-            <button class="category__btn category__del" aria-label="удалить"></button>
-          </li>
-
-          <li class="category__item" data-id="bc93pfmb4nhw">
-            <button class="category__card">
-              <span class="category__title">Притяжательные местоимения</span>
-              <span class="category__pairs">6 пар</span>
-            </button>
-            <button class="category__btn category__edit" aria-label="редактировать"></button>
-            <button class="category__btn category__del" aria-label="удалить"></button>
-          </li>
-
-          <li class="category__item" data-id="bczp358gktzy">
-            <button class="category__card">
-              <span class="category__title">Семья</span>
-              <span class="category__pairs">20 пар</span>
-            </button>
-            <button class="category__btn category__edit" aria-label="редактировать"></button>
-            <button class="category__btn category__del" aria-label="удалить"></button>
-          </li>
-        </categoryList>
-      </div>
-    </section>
-*/
